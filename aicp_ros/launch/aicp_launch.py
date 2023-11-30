511eb92716da726d616b556c66c7091a1a37f620
m.lisi1 <m.lisi1@studenti.unipi.it>
2023-11-30 14:29:45 +0100

    was spinning wrong node

--- aicp_ros/launch/aicp_launch.py
+++ aicp_ros/launch/aicp_launch.py
@@ -16,11 +16,25 @@ def generate_launch_description():
             package='aicp_ros',
             executable='aicp_ros_node',
             output='screen',
+            arguments= [
+                "--ros-args",
+                "--log-level",
+                "aicp_ros_node:=info",
+            ],
             parameters=[{
                 'registration_config_file': os.path.join(get_prefix_path("aicp_core"), 'share', "aicp_core", "config/icp/icp_autotuned.yaml"),
                 'aicp_config_file' :  os.path.join(get_prefix_path("aicp_core"), 'share', "aicp_core", "config/aicp_config.yaml"),
                 'registration_default_config_file' : os.path.join(get_prefix_path("aicp_core"), 'share', "aicp_core", "/config/icp/icp_autotuned_default.yaml"),
-                 
+                 'verbose' : True,
             },]
         ),
+
+        Node(
+            package='velodyne_streamer',
+            executable='streamer',
+            remappings=[('cloud_in', '/point_cloud_filter/velodyne/point_cloud_filtered')],
+            parameters=[{
+                'publish_rate': 30
+            }]
+        )
     ])
